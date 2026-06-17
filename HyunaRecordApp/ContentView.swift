7964bdbd31e8894933c188d09f569e4ec8849c7a
Hyuna Cho <cho-hyuna@yadokari.tv>
2026-06-17 15:49:29 +0900

    Initial Commit

---
+++ HyunaRecordApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HyunaRecordApp
+//
+//  Created by Cho Hyuna on 2026/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HyunaRecordApp/HyunaRecordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyunaRecordAppApp.swift
+//  HyunaRecordApp
+//
+//  Created by Cho Hyuna on 2026/06/17.
+//
+
+import SwiftUI
+
+@main
+struct HyunaRecordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
